3b4e066fc2816f60b1de74c496363a9187da46f8
Soner Kurnaz <sonerkurnaz@outlook.com>
2022-06-21 13:12:54 +0300

    data transfer

---
+++ DotNetCore.DataTransfer.ToView/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using DotNetCore.DataTransfer.ToView.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCore.DataTransfer.ToView.Controllers
+{
+    public class ProductController : Controller
+    {
+        public List<Product> GetProductList
+        {
+            get
+            {
+
+            }
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
