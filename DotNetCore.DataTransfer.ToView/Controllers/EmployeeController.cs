bf947c1a3a6a2b7f33a3cfcc1e25497e58499559
Soner Kurnaz <sonerkurnaz@outlook.com>
2022-06-22 01:38:41 +0300

    employeecont

---
+++ DotNetCore.DataTransfer.ToView/Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCore.DataTransfer.ToView.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- DotNetCore.DataTransfer.ToView/Controllers/ProductController.cs
+++ DotNetCore.DataTransfer.ToView/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using DotNetCore.DataTransfer.ToView.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace DotNetCore.DataTransfer.ToView.Controllers
 {
@@ -9,12 +10,34 @@ namespace DotNetCore.DataTransfer.ToView.Controllers
         {
             get
             {
+                List<Product> products = new List<Product>();
 
+                products.Add(new Product { Id = 1, ProductName = "Elma", Price = 21, Stock = 10 });
+                products.Add(new Product { Id = 2, ProductName = "Armut", Price = 22, Stock = 10 });
+                products.Add(new Product { Id = 3, ProductName = "Karpuz", Price = 23, Stock = 10 });
+                products.Add(new Product { Id = 4, ProductName = "Kavun", Price = 24, Stock = 10 });
+
+                return products;
             }
+        }
+
+        public IActionResult List()
+        {
+            TempData["ProductList"] = JsonConvert.SerializeObject(GetProductList);
+            ViewBag.ProductList = JsonConvert.SerializeObject(GetProductList);
+            ViewData["ProductList"] = JsonConvert.SerializeObject(GetProductList);
+
+            return RedirectToAction("Index");
         }
         public IActionResult Index()
         {
-            return View();
+            var tempDataVeri = TempData["ProductList"];
+            var viewBagDataVeri = ViewBag.ProductList;
+            var viewDataVeri = ViewData["ProductList"];
+
+
+
+            return View(tempDataVeri);
         }
     }
 }
