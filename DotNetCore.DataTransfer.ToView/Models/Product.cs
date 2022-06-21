b739eb7c22359d10798e73c538972442acc2fa91
Soner Kurnaz <sonerkurnaz@outlook.com>
2022-06-21 12:33:40 +0300

    Add project files.

---
+++ DotNetCore.DataTransfer.ToView/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace DotNetCore.DataTransfer.ToView.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+
+    }
+}
